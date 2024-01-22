d76af63afa1a136eccf68d89f11527c404c2df03
YuMoua <153407360+YuMoua@users.noreply.github.com>
2024-01-21 20:49:01 -0600

    updated

--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,98 @@
+let startContainerEl = document.getElementById('startContainer')
+let timerEl = document.getElementById('timer');
+let startButtonEl = document.getElementById('startButton');
+let quizContainerEl = document.getElementById('quizContainer');
+let questionAndAnswerBlockEl = document.getElementById('questionAndAnswerBlock');
+let questionEl = document.getElementById('question');
+let choiceContainerEl = document.getElementById('choiceContainer');
+
+let totalScore = 0;
+let timeLeft = 30;
+let questionAmount = 3;
+
 // GIVEN I am taking a code quiz
 // WHEN I click the start button
 // THEN a timer starts and I am presented with a question
 
-let timeLeft = 30;
-function countDown() {
-    for (let i = timeLeft; i <= 0; timeLeft--) {
-        console.log(timeLeft);
-    }
+startButtonEl.addEventListener('click', function () {
+    timerEl.textContent = timeLeft + ' Seconds Remaining';
+    startButtonEl.remove();
+    correctOrWrong.textContent = '';
+    firstQuestion();
+});
 
-    return;
-}
-let countInterval = setInterval(countDown, 1000);
-setTimeout(function() {
-    clearInterval(countInterval);
 
-}, 1000)
+startButtonEl.addEventListener('click', function () {
 
-setInterval(countDown, 1000)
+    let timeInterval = setInterval(function () {
+        if (timeLeft !== 0) {
+            timeLeft--;
+            // setTimerText();
+
+        } else {
+            timerEl.textContent = '';
+            clearInterval(timeInterval);
+            displayMessage();
+            return;
+        }
+    }, 1000);
+
+    timerEl = timeLeft;
+    return timerEl;
+});
+
+startButtonEl.addEventListener('click', nextQuestion());
+
+
+
+function displayMessage() {
+    let message = 'Time is up!!!';
+
+    if (timerEl === 0) {
+        questionEl = message
+
+    };
+    return questionEl;
+};
+
 
 // WHEN I answer a question
 // THEN I am presented with another question
+function firstQuestion() {
+    choiceContainerEl.appendChild(div)
 
+};
+function nextQuestion() {
+
+};
 
 
 // WHEN I answer a question incorrectly
 // THEN time is subtracted from the clock
 
+// if (choiceButtonEl === 0) {
+//     totalScore--;
+// };
 
 
 // WHEN all questions are answered or the timer reaches 0
 // THEN the game is over
 
+if (questionAmount === 0) {
+
+    startButtonEl.textContent = '';
+    correctOrWrong.textContent = '';
+    // choiceContainer.removeChild(choiceButtonEl); //Need to find a way to remove choice buttons
+    // questionEl.textContent = 'You have scored: ' + totalScore;
+
+};
+
+if (timeLeft === 0) {
+    startButtonEl.textContent = '';
+    correctOrWrong.textContent = '';
+    choiceButtonEl.remove();
+    questionEl.textContent = 'You have scored: ' + totalScore;
+};
 
 
 // WHEN the game is over
